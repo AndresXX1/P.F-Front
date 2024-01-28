47f3fc77b5dc6c2d584ebd04a71cc45d60dbcfc5
MQuirico <128657405+MQuirico@users.noreply.github.com>
2024-01-27 23:13:48 -0300

    ultimos cambios

---
+++ src/componentes/DashBoard Usuario/Métodos de pago/paymentMethods.jsx
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import './PaymentMethods.css';
+import Diners from '../../../assets/diners-club-international-tarjeta.png';
+import MasterCard from '../../../assets/Master.jpg';
+import Visa from '../../../assets/VisaT.png';
+import Naranja from '../../../assets/Naranja.jpg';
+import Amex from '../../../assets/Amex.jpg';
+import Cabal from '../../../assets/cabal-logo.png'
+
+export default function PaymentMethods () {
+  const [selectedBrand, setSelectedBrand] = React.useState("");
+  const {register, formState: { errors }, watch, setValue ,handleSubmit} = useForm(); 
+  const handleKeyPress1 = (event) => {
+    if (!/[0-9]/.test(event.key) || event.target.value.length >= 16) {
+       event.preventDefault();
+    }
+   };
+
+   const handleKeyPress2 = (event) => {
+    if (!/[0-9]/.test(event.key) || event.target.value.length >= 3) {
+       event.preventDefault();
+    }
+   };
+
+   const onSubmit = (data, e) => {
+    e.preventDefault();
+    console.log(data)
+    Object.keys(data).forEach((fieldName) => {
+      setValue(fieldName, '');
+  });
+   }
+
+   const handleSelectChange = (event) => {
+    setSelectedBrand(event.target.value);
+};
+
+    return(
+        <>
+            <div className="payment-container">
+      <h3>Metodos de pago asociados a tu cuenta:</h3> 
+      <div className="payment-form">
+        <h3>Aquí podrás gestionar los métodos de pago asociados a tu cuenta.</h3>
+        <p>Podrás agregar tarjetas de crédito así como también de débito</p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-group">
+            <label>Seleccione la marca de su tarjeta de crédito</label>
+            <select {...register('marca', { required: true })} className="select-box" onChange={handleSelectChange}>
+              <option value="">Seleccione una opción</option>
+              <option value='Visa'>Visa</option>
+              <option value='MasterCard'>MasterCard</option>
+              <option value='Naranja'>Naranja</option>
+              <option value='American Express'>American Express</option>
+              <option value='Diners Club'>Diners Club</option>
+              <option value='Cabal'>Cabal</option>
+            </select>
+            {errors.marca?.type === 'required' && <p className="error">Por favor, seleccione una marca de Tarjeta de Crédito.</p>}
+          </div>
+
+          <div className="form-group">
+            <label>N° de tarjeta de crédito</label>
+            <input type="text" onKeyPress={handleKeyPress1} {...register('nroTarjeta', { required: true, maxlength: 16 })} className="text-input" />
+            {errors.nroTarjeta?.type === 'required' && <p className="error">Por favor, ingrese el n° de su Tarjeta de Crédito.</p>}
+          </div>
+
+          <div className="form-group">
+            <label>CVV (Code Verification Value)</label>
+            <p>*puedes verificarlo en el dorso de tu tarjeta*</p>
+            <input type='text' onKeyPress={handleKeyPress2} {...register('cvv', { required: true, maxlength: 3 })} className="text-input" />
+            {errors.cvv?.type === 'required' && <p className="error">Por favor, ingrese el código de verificación al dorso de su tarjeta de crédito.</p>}
+          </div>
+
+          <h3>Tarjeta:</h3>
+          {selectedBrand === 'Visa' && <img src={Visa}></img>}
+          {selectedBrand === 'MasterCard' && <img src={MasterCard}></img>}
+          {selectedBrand === 'Naranja' && <img src={Naranja}></img>}
+          {selectedBrand === 'American Express' && <img src={Amex}></img>}
+          {selectedBrand === 'Diners Club' && <img src={Diners}></img>}
+          {selectedBrand === 'Cabal' && <img src={Cabal}></img>}
+          <button type="submit" className="submit-button">Guardar</button>
+        </form>
+      </div>
+    </div>  
+        </>
+    )
+}
